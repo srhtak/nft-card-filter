8d78d51062b23133e3d46b643c2ee3d10a251ded
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-26 17:35:03 +0300

    modified text

--- src/components/FilterDropdown.vue
+++ src/components/FilterDropdown.vue
@@ -3,7 +3,7 @@
     <div>
       <div class="dropdown relative">
         <button @click="dropdown = true" class="btn-dropdown" type="button">
-          Dropdown button
+          Sort
           <svg
             aria-hidden="true"
             focusable="false"
@@ -30,7 +30,7 @@
               @click="sortingDate"
               class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
             >
-              Sorting date
+              Sort by date
             </button>
           </li>
           <li>
@@ -38,7 +38,7 @@
               @click="sortingPrice"
               class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
             >
-              Sorting price
+              Sort by price
             </button>
           </li>
         </ul>
