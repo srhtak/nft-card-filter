32b923c9409c1d6ac14a4c4a8605be7eaa6e430a
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-27 13:10:28 +0300

    width and height classes added out container

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,10 +18,12 @@ onMounted(() => {
 
 <template>
   <NftFilterForm @filter="filterByForm" />
-  <div v-auto-animate class="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+  <div
+    v-auto-animate
+    class="grid md:grid-cols-2 lg:grid-cols-3 w-full h-screen gap-8"
+  >
     <div v-for="item in filterData" :key="item.id">
       <NftCard :item="item" />
     </div>
   </div>
-  <div></div>
 </template>
